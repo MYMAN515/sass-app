e099ddfcd3d5990d4ebd75e763efab0042dfa71d
MYMAN515 <dr.rakradi@gmail.com>
2025-08-05 17:39:57 +0400

    Update enhance.js

--- pages/api/enhance.js
+++ pages/api/enhance.js
@@ -1,10 +1,10 @@
 // pages/api/enhance.js
 
 import { createClient } from '@supabase/supabase-js';
-
+//
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY // هذا لازم يكون service role token
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY // هذا لازم يكون service role token
 );
 
 export default async function handler(req, res) {
