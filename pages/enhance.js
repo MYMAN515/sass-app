96ebc867053a8a6e817e76a24b98fee4d213d736
MYMAN515 <dr.rakradi@gmail.com>
2025-08-05 23:24:36 +0400

    Update enhance.js

--- pages/enhance.js
+++ pages/enhance.js
@@ -13,7 +13,7 @@ import { Poppins } from 'next/font/google';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '600', '700'], display: 'swap' });
 
-function generateEnhancePrompt = ({
+const generateEnhancePrompt = ({
   photographyStyle = '',
   background = '',
   lighting = '',
