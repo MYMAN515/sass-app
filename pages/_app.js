822490ff5be7c7a654fa43490fe49338794fae56
MYMAN515 <dr.rakradi@gmail.com>
2025-08-10 20:42:26 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,21 +4,42 @@ import { Montserrat, Open_Sans } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 
 import { useState } from 'react';
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
-const montserrat = Montserrat({ subsets: ['latin'], weight: ['600', '800'], variable: '--font-montserrat' });
-const openSans   = Open_Sans({ subsets: ['latin'], weight: ['400', '600'], variable: '--font-open-sans' });
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['600', '800'],
+  variable: '--font-montserrat',
+});
+
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['400', '600'],
+  variable: '--font-open-sans',
+});
 
 export default function App({ Component, pageProps }) {
-  const [supabaseClient] = useState(() => createPagesBrowserClient());
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
   return (
     <SessionContextProvider supabaseClient={supabaseClient} initialSession={pageProps.initialSession}>
-      <main className={`${montserrat.variable} ${openSans.variable} font-sans bg-[#0f0c29] min-h-screen text-white`}>
+      {/* Vercel Analytics */}
+      <Analytics />
+
+      {/* Main wrapper applies fonts and dark bg */}
+      <main
+        className={`
+          ${montserrat.variable}
+          ${openSans.variable}
+          font-sans
+          bg-[#0f0c29]
+          min-h-screen
+          text-white
+        `}
+      >
         <Component {...pageProps} />
       </main>
-      <Analytics />
     </SessionContextProvider>
   );
 }
