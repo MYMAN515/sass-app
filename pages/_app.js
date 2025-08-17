f935c169d22d5cc14d25d7e49a8c7255bed6573f
MYMAN515 <dr.rakradi@gmail.com>
2025-08-17 22:17:27 +0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,11 @@
 // /pages/_app.js
 import '@/styles/globals.css';
-import { Montserrat, Open_Sans } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 
 import { useState } from 'react';
 // ✅ الصحيح مع Pages Router
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
-
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['600', '800'],
-  variable: '--font-montserrat',
-});
-
-const openSans = Open_Sans({
-  subsets: ['latin'],
-  weight: ['400', '600'],
-  variable: '--font-open-sans',
-});
 
 export default function App({ Component, pageProps }) {
   // أنشئ عميل واحد فقط طوال عمر التطبيق
@@ -30,14 +17,7 @@ export default function App({ Component, pageProps }) {
       initialSession={pageProps.initialSession}
     >
       <Analytics />
-      <div
-        className={`
-          ${montserrat.variable} ${openSans.variable}
-          font-sans bg-[#0f0c29] min-h-screen text-white
-        `}
-      >
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </SessionContextProvider>
   );
 }
