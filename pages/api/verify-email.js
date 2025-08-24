72bfc5dae522cdfd1812bb3694cb2631b48f3c28
MYMAN515 <dr.rakradi@gmail.com>
2025-08-24 15:59:22 +0400

    Update verify-email.js

--- pages/api/verify-email.js
+++ pages/api/verify-email.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import Cookies from 'js-cookie';
 import { motion, AnimatePresence } from 'framer-motion';
