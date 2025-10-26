b9fe39b6f14d95fa7cf910a267d163a2c4a2b83e
MYMAN515 <dr.rakradi@gmail.com>
2025-10-26 10:22:54 +0800

    Simplify landing page design

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 // pages/index.js
 import Layout from '@/components/Layout';
-import dynamic from 'next/dynamic';
-
-const HeroSection = dynamic(() => import('@/components/HeroSection'), {
-  ssr: false
-});
+import HeroSection from '@/components/HeroSection';
 
 export default function HomePage() {
   return (
-    <Layout>
+    <Layout className="px-0 md:px-0">
       <HeroSection />
     </Layout>
   );
